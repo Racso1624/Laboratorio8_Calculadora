fd7193b24c3b0d823b573e51e038866dffe7f150
Racso1624 <fer10_lb@outlook.com>
2022-05-16 12:11:53 -0600

    Se agregan funcionalidades

--- src/index.js
+++ src/index.js
@@ -8,7 +8,9 @@ const App = () =>{
     const [valor_1, setValor_1] = React.useState("")
     const [valor_2, setValor_2] = React.useState("")
     const [expresion, setExpresion] = React.useState("")
+    const [operacion, setOperacion] = React.useState("")
     const [resultado, setResultado] = React.useState("")
+    const [secuencia, setSecuencia] = React.useState(false)
     const botones = ["C", "+/-", "MOD", "÷", "7", "8", "9", "x", "4", "5", "6", "-", "1", "2", "3", "+", "0", ".", "=", "DEL"]
     const numeros = ["7", "8", "9", "4", "5", "6", "1", "2", "3", "0"]
 
@@ -30,12 +32,64 @@ const App = () =>{
                                     if(expresion_nueva.length !== 10){
                                         setExpresion(expresion_nueva)
                                     }
-
                                 }
                             }>{elemento}</button>
                         }
                         else{
-                            return <button className="boton" key={posicion.toString()}>{elemento}</button>
+                            return <button 
+                            className="boton" 
+                            key={posicion.toString()}
+                            onClick={
+                                () =>{
+                                    if(elemento === "+" && elemento === "-" && elemento === "*" && elemento === "÷" && elemento === "MOD"){
+
+                                        setValor_1(expresion)
+                                        setExpresion("")
+                                        setOperacion(elemento)
+
+                                        // else{
+                                        //     var valor1 = Number(valor_1)
+                                        //     var valor2 = Number(expresion)
+                                        //     var resultado = valor1 + valor2
+                                        //     setValor_1("")
+                                        //     setResultado(resultado)
+                                        //     setExpresion(resultado)
+                                        //     setSecuencia(true)
+                                        // }
+                                    }
+                                    else if(elemento === "."){
+                                        var expresion_nueva = ""
+                                        if(!expresion.includes(".")){
+                                            expresion_nueva = expresion + elemento
+                                            if(expresion_nueva.length !== 10 ){
+                                                setExpresion(expresion_nueva)
+                                            }
+                                        }
+                                    }
+                                    else if(elemento === "DEL"){
+                                        var expresion_nueva = expresion.slice(0, -1)
+                                        setExpresion(expresion_nueva)
+                                    }
+                                    else if(elemento === "C"){
+                                        setExpresion("")
+                                    }
+                                    else if(elemento === "="){
+                                        if(elemento === "+"){
+                                            
+                                        }
+                                        else if(elemento === "-"){
+
+                                        }
+                                        else if(elemento === "*"){
+
+                                        }
+                                        else if(elemento === "÷"){
+                                            
+                                        }
+                                    }
+                                    
+                                }
+                            }>{elemento}</button>
                         }
                     })
                 }
